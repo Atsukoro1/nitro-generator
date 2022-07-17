15a6c8695298c7d42dda52be27131a9f1b645738
Atsukoro1 <dornicakkuba@gmail.com>
2022-07-17 21:41:18 +0200

    Spawn thread

--- src/main.rs
+++ src/main.rs
@@ -5,54 +5,56 @@ mod proxy;
 mod code;
 mod menu;
 
-#[tokio::main]
+#[tokio::main(worker_threads = 1)]
 async fn main() -> std::io::Result<()> {
-    menu::draw_menu().await;
     let mut proxies: Vec<proxy::Proxy> = vec![];
     let mut generated: Option<u128> = Some(1);
 
-    output::print_menu();
     let config: menu::Settings = menu::draw_menu().await;
 
-    while config.code_count >= generated.unwrap() { 
-        // Fetch proxies when proxy list is empty
-        if proxies.len() == 0 {
-            proxies = proxy::fetch_proxies(&config.proxy_souce)
-                .await
-                .expect("Failed to fetch proxies");
+    tokio::spawn(async move {
+        while config.code_count >= generated.unwrap() { 
+            // Fetch proxies when proxy list is empty
+            if proxies.len() == 0 {
+                proxies = proxy::fetch_proxies(&config.proxy_souce)
+                    .await
+                    .expect("Failed to fetch proxies");
+            };
+    
+            // Generate code
+            let code: String = code::generate_code(&config);
+    
+            // Check the code
+            let proxy: &proxy::Proxy = proxies.first().unwrap();
+    
+            let proxy_string: String = format!("{}:{}", proxy.host, proxy.port);
+            let response: reqwest::StatusCode = code::check_code(&code, &proxy_string).await;
+    
+            match response {
+                reqwest::StatusCode::OK => {
+                    output::print_success(&code);
+                },
+    
+                reqwest::StatusCode::NOT_FOUND => {
+                    output::print_error(&code);
+                }
+    
+                reqwest::StatusCode::TOO_MANY_REQUESTS => {
+                    output::print_ratelimit(&code);
+                    proxies.drain(0..1);
+                }
+    
+                _ => {
+                    output::print_invalid_proxy(&code);
+                    proxies.drain(0..1);
+                }
+            }
+    
+            generated = Some(generated.unwrap() + 1);
         };
-
-        // Generate code
-        let code: String = code::generate_code(&config);
-
-        // Check the code
-        let proxy: &proxy::Proxy = proxies.first().unwrap();
-
-        let proxy_string: String = format!("{}:{}", proxy.host, proxy.port);
-        let response: reqwest::StatusCode = code::check_code(&code, &proxy_string).await;
-
-        match response {
-            reqwest::StatusCode::OK => {
-                output::print_success(&code);
-            },
-
-            reqwest::StatusCode::NOT_FOUND => {
-                output::print_error(&code);
-            }
-
-            reqwest::StatusCode::TOO_MANY_REQUESTS => {
-                output::print_ratelimit(&code);
-                proxies.drain(0..1);
-            }
-
-            _ => {
-                output::print_invalid_proxy(&code);
-                proxies.drain(0..1);
-            }
-        }
-
-        generated = Some(generated.unwrap() + 1);
-    };
+    })
+    .await
+    .expect("Failed to create thread");
 
     Ok(())
 }
